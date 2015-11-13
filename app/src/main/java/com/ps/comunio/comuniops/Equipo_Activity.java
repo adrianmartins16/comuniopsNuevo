29d85794409ad2490856df723a47137c4439cf96
Hawk <alvaro.pavon.alvarado@gmail.com>
2015-11-13 22:24:48 +0100

    Pequeño ajuste en las pestañas de Home.

--- app/src/main/java/com/ps/comunio/comuniops/Equipo_Activity.java
+++ app/src/main/java/com/ps/comunio/comuniops/Equipo_Activity.java
@@ -1,16 +1,60 @@
 package com.ps.comunio.comuniops;
 
+import android.content.Intent;
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TabHost;
+import android.widget.Toast;
 
 public class Equipo_Activity extends AppCompatActivity {
+    private ListView plantilla;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_equipo_);
+
+        //Pestañas de equipo
+        Resources res1 = getResources();
+        TabHost tabs = (TabHost) findViewById(android.R.id.tabhost);
+        tabs.setup();
+        TabHost.TabSpec spec = tabs.newTabSpec("Pestaña 1");
+        spec.setContent(R.id.tab3);
+        spec.setIndicator("Plantilla");
+        tabs.addTab(spec);
+
+        tabs.setup();
+        TabHost.TabSpec spec1 = tabs.newTabSpec("Pestaña 2");
+        spec1.setContent(R.id.tab4);
+        spec1.setIndicator("Alineación");
+        tabs.addTab(spec1);
+
+        //Plantilla
+        final String[] miplantilla = {"Tono", "Victor", "Felix", "Alvaro", "Adrian","Uafi", "Carlos", "Superman", "Mandril", "Torres"};
+
+        plantilla = (ListView)findViewById(R.id.listView);
+        ArrayAdapter<String> adaptador = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, miplantilla);
+
+        plantilla.setAdapter(adaptador);
+        plantilla.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int position, long id) {
+
+                Toast.makeText(getApplicationContext(), "Cargando: " + miplantilla[position], Toast.LENGTH_SHORT).show();
+                Intent i = new Intent(Equipo_Activity.this, Jugador_Activity.class);
+                i.putExtra("jugador", position);
+                startActivity(i);
+            }
+
+        });
     }
 
     @Override
