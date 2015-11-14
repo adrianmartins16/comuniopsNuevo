9221dec0abacc689b2ff88c796082e2567a50589
vmunore <v.munore@alumnos.urjc.es>
2015-11-14 13:37:50 +0100

    Login cambiado y registro hecho

--- app/src/main/java/com/ps/comunio/comuniops/Login_Activity.java
+++ app/src/main/java/com/ps/comunio/comuniops/Login_Activity.java
@@ -3,6 +3,7 @@ package com.ps.comunio.comuniops;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -10,39 +11,51 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class Login_Activity extends AppCompatActivity {
-    String username = "admin";
-    String password = "1234";
+
+    Usuarios_DB admin = new Usuarios_DB("admin","1234");
     Button btLogin;
+    Button btRegistro;
     EditText etUser;
     EditText etPass;
+    ArrayList<Usuarios_DB> listaUsuarios;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        listaUsuarios.add(admin);
+        if((getIntent().getStringExtra("user")!=null)&&(getIntent().getStringExtra("pass")!=null)){
+            String username = getIntent().getStringExtra("user");
+            String password = getIntent().getStringExtra("pass");
+            Usuarios_DB nuevo = new Usuarios_DB(username,password);
+            listaUsuarios.add(nuevo);
+        }
+
 
         etUser = (EditText) findViewById(R.id.editTextUserLogin);
         etPass = (EditText) findViewById(R.id.editTextPassLogin);
         btLogin = (Button) findViewById(R.id.buttonLoginLogin);
+
         btLogin.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 Log.d("Login", "Boton Login pulsado");
                 //Log.d("Login", String.valueOf(tvUser.getText()));
-                if (String.valueOf(etUser.getText()).equals(username)) {
+                String sacarUsername = etUser.getText().toString();
+                String sacarPassword = etPass.getText().toString();
+                Usuarios_DB comprobador = new Usuarios_DB(sacarUsername,sacarPassword);
+
+                if (tieneUsuario(comprobador)) {
                     Log.d("Login", "Usuario Valido");
-                    if (String.valueOf(etPass.getText()).equals(password)) {
-                        Log.d("Login", "Pass Valido");
-                        Intent i = new Intent(Login_Activity.this, Home_Activity.class);
-                        i.putExtra("user", etUser.getText() + "");
+                    Log.d("Login", "Pass Valido");
+                    Intent i = new Intent(Login_Activity.this, Home_Activity.class);
+                    i.putExtra("user", etUser.getText() + "");
 
-                        startActivity(i);
-                    }else{
-                        Toast.makeText(Login_Activity.this, "Usuario o Contraseña erróneos.",
-                                Toast.LENGTH_SHORT).show();
-
-                    }
+                    startActivity(i);
+                    finish();
                 }else{
                     Toast.makeText(Login_Activity.this, "Usuario o Contraseña erróneos.",
                             Toast.LENGTH_SHORT).show();
@@ -50,7 +63,28 @@ public class Login_Activity extends AppCompatActivity {
                 }
             }
         });
+        btRegistro = (Button) findViewById(R.id.buttonRegistrarLogin);
+        btRegistro.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
 
+                    Intent i = new Intent(Login_Activity.this, Registrar_Activity.class);
+                    startActivity(i);
+                    finish();
+
+            }
+        });
+
+
+    }
+
+    public Boolean tieneUsuario(Usuarios_DB userPasado){
+        for (Usuarios_DB i:listaUsuarios) {
+            if ((i.getUsername().equals(userPasado.getUsername()))&&(i.getPass().equals(userPasado.getPass()))){
+                return Boolean.TRUE;
+            }
+        }
+        return Boolean.FALSE;
     }
     //Boton Login
 
---
+++ app/src/main/java/com/ps/comunio/comuniops/Registrar_Activity.java
@@ -0,0 +1,50 @@
+package com.ps.comunio.comuniops;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Registrar_Activity extends AppCompatActivity {
+    EditText etUserNew;
+    EditText etPassNew;
+    EditText etPass2New;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar_);
+
+        Button registrar = (Button) findViewById(R.id.buttonregistarregistrar);
+        etUserNew = (EditText) findViewById(R.id.editTextusername);
+        etPassNew = (EditText) findViewById(R.id.editTextpass1);
+        etPass2New = (EditText) findViewById(R.id.editTextpass2);
+
+        registrar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Log.d("Login", "Boton Login pulsado");
+                //Log.d("Login", String.valueOf(tvUser.getText()));
+                String aux = String.valueOf(etPass2New.getText());
+                if (String.valueOf(etPassNew.getText()).equals(aux)) {
+                    Toast.makeText(Registrar_Activity.this, "Usuario Registrado!",
+                            Toast.LENGTH_SHORT).show();
+
+                        Intent i = new Intent(Registrar_Activity.this, Login_Activity.class);
+                        i.putExtra("user", etUserNew.getText());
+                        i.putExtra("pass", etPassNew.getText());
+                        startActivity(i);
+                        finish();
+
+                }else{
+                    Toast.makeText(Registrar_Activity.this, "Las contraseñas no coinciden.",
+                            Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+    }
+}
