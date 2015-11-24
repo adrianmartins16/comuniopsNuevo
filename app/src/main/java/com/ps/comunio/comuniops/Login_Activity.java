55c4379de782eb0921aec5e4b192cc8660a0699c
Hawk <alvaro.pavon.alvarado@gmail.com>
2015-11-24 14:31:41 +0100

    No funciona nada :(

--- app/src/main/java/com/ps/comunio/comuniops/Login_Activity.java
+++ app/src/main/java/com/ps/comunio/comuniops/Login_Activity.java
@@ -1,17 +1,38 @@
 package com.ps.comunio.comuniops;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Editable;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
+
 
 public class Login_Activity extends AppCompatActivity {
 
@@ -21,11 +42,22 @@ public class Login_Activity extends AppCompatActivity {
     EditText etUser;
     EditText etPass;
     ArrayList<Usuarios_DB> listaUsuarios = new ArrayList<>();
+    private ProgressDialog progressDialog;
+    private String text = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        try {
+            text = conexionURL("http://comuniops.hol.es/index.php?funcion=checkLogin&username=reiner&password=reiner");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Toast.makeText(Login_Activity.this, text,
+                Toast.LENGTH_SHORT).show();
+
         listaUsuarios.add(admin);
         if((getIntent().getStringExtra("user")!=null)&&(getIntent().getStringExtra("pass")!=null)){
             String username = getIntent().getStringExtra("user");
@@ -78,7 +110,32 @@ public class Login_Activity extends AppCompatActivity {
 
 
     }
+    public String conexionURL(String urlString) throws IOException {
+        URL url = new URL("http://www.android.com/");
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream
+            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+             return readStream(in);
 
+        }finally {
+            urlConnection.disconnect();
+        }
+
+    }
+    private String readStream(InputStream is) {
+        try {
+            ByteArrayOutputStream bo = new ByteArrayOutputStream();
+            int i = is.read();
+            while(i != -1) {
+                bo.write(i);
+                i = is.read();
+            }
+            return bo.toString();
+        } catch (IOException e) {
+            return "";
+        }
+    }
     public Boolean tieneUsuario(Usuarios_DB userPasado){
         for (Usuarios_DB i:listaUsuarios) {
             if ((i.getUsername().equals(userPasado.getUsername()))&&(i.getPass().equals(userPasado.getPass()))){
